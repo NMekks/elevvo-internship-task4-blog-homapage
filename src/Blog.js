5ba437766bcd104ad5e39f2003794315c7595da4
NMekks <nusaibamekka@gmail.com>
2025-08-25 03:06:03 +0300

    fixed navbar, added blog info, added searchbar front-end

--- src/Blog.js
+++ src/Blog.js
@@ -3,12 +3,28 @@ import React from "react";
 const Blog = () => {
   return (
     <div className="blog-homepage">
-        <div className="blog-image mx-10 w-60">
-            <img src="/blog-image.jpg" alt="" />
-        </div>
-
-    
-   
+        <div className="search-bar flex mt-8 justify-center">
+        <div className="search w-[50%] py-1 px-4 text-xl text-gray-300 border border-gray-300 rounded-lg bg-white">Search</div>
+      </div>
+      <div className="blog-image mx-12 mt-5 mb-8 w-60">
+        <img src="/blog-image.jpg" alt="" />
+      </div>
+      <div className="blog-intro">
+        <p className="w-64 justify-center mx-12">
+          Welcome to my blog where i share my experiences with you all! Thank
+          you checking out my page!
+        </p>
+      </div>
+      <div className="socials mx-12 my-4">
+        <a href="#">Twitter |</a>
+        <a href="#">Tumblr |</a>
+        <a href="#">Instagram |</a>
+        <a href="#">TikTok</a>
+      </div>
+      <div className="FAQ ">
+        <button className="FAQ-btn rounded-lg bg-amber-900 text-white px-24 py-1 mx-12">F.A.Q.</button>
+      </div>
+      
     </div>
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,11 +3,11 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="page-header flex justify-between items-center p-4 text-white bg-slate-800">
-        <h1 className="title text-3xl ms-5">
+      <div className="page-header flex justify-between items-center px-4 py-2 bg-orange-400">
+        <h1 className="title text-3xl ms-5 text-white">
           My Coffee Mug
         </h1>
-        <button className="btn-add-post rounded-md bg-slate-400 py-2 px-4 me-5">
+        <button className="btn-add-post rounded-md bg-amber-900 text-white py-2 px-4 me-5">
           Add Post +
         </button>
       </div>
