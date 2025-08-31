ae6fc5c9408875eb0cd8018c63bc736cb686101d
NMekks <nusaibamekka@gmail.com>
2025-08-31 21:36:02 +0300

    adjusted styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="page-header flex justify-between items-center px-4 py-2">
-        <h1 className="title text-3xl ms-5 text-amber-900 font-semibold">
+        <h1 className="title text-3xl ms-5 text-amber-900 font-semibold tracking-wide">
           My Coffee Mug
         </h1>
         <button className="btn-add-post rounded-md bg-amber-900 text-white py-2 px-4 me-5">
